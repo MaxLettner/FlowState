1a0b4758d1ff2ad440996ca2451c24d61cb925b8
Jonsonrath <jonathan.rath@gmx.at>
2026-04-07 07:59:57 +0200

    refactor: Deleted Requirements for Skills

--- src/main/java/at/htl/flowstate/Skills/SkillList.java
+++ src/main/java/at/htl/flowstate/Skills/SkillList.java
@@ -10,69 +10,69 @@ public class SkillList {
         this.skills = new Skill[]{
 
                 // Top level
-                new Skill("Magic skill tree", 0, 0, 0, SkillType.MAGIC),
-                new Skill("Meele skill tree", 0, 0, 0, SkillType.MEELE),
-                new Skill("Ranged skill tree", 0, 0, 0, SkillType.RANGED),
+                new Skill("Magic skill tree", SkillType.MAGIC),
+                new Skill("Meele skill tree", SkillType.MEELE),
+                new Skill("Ranged skill tree", SkillType.RANGED),
 
                 // Magic sub-trees
-                new Skill("Arcane magic", 0, 0, 0, SkillType.MAGIC_ARCANE),
-                new Skill("Enchanting magic", 0, 0, 0, SkillType.MAGIC_ENCHANTING),
-                new Skill("Elemental magic", 0, 0, 0, SkillType.MAGIC_ELEMENTAL),
+                new Skill("Arcane magic", SkillType.MAGIC_ARCANE),
+                new Skill("Enchanting magic", SkillType.MAGIC_ENCHANTING),
+                new Skill("Elemental magic", SkillType.MAGIC_ELEMENTAL),
 
                 // Arcane skills
-                new Skill("Flying with continuous mana use", 0, 0, 0, SkillType.ARCANE_LEVITATION),
-                new Skill("Homing Magic Missile, low DMG", 0, 0, 0, SkillType.ARCANE_MAGIC_MISSILE),
-                new Skill("Reduces damage taken, continuous mana use", 0, 0, 0, SkillType.ARCANE_MANA_SHIELD),
+                new Skill("Flying with continuous mana use", SkillType.ARCANE_LEVITATION),
+                new Skill("Homing Magic Missile, low DMG", SkillType.ARCANE_MAGIC_MISSILE),
+                new Skill("Reduces damage taken, continuous mana use", SkillType.ARCANE_MANA_SHIELD),
 
                 // Elemental skills
-                new Skill("Straight flying missile, AOE damage over time", 0, 0, 0, SkillType.ELEMENTAL_FIRE_BALL),
-                new Skill("Shoots icecicle, medium mana cost", 0, 0, 0, SkillType.ELEMENTAL_ICECICLE),
-                new Skill("Stackable DMG over time, low mana cost", 0, 0, 0, SkillType.ELEMENTAL_POISON_DARTS),
+                new Skill("Straight flying missile, AOE damage over time", SkillType.ELEMENTAL_FIRE_BALL),
+                new Skill("Shoots icecicle, medium mana cost", SkillType.ELEMENTAL_ICECICLE),
+                new Skill("Stackable DMG over time, low mana cost", SkillType.ELEMENTAL_POISON_DARTS),
 
                 // Enchanting skills
-                new Skill("Fire Aspect enchantment", 0, 0, 0, SkillType.ENCHANTING_FIRE_ASPECT),
-                new Skill("Steals health based on outputted DMG", 0, 0, 0, SkillType.ENCHANTING_LIFE_STEAL),
-                new Skill("Adds pierce to projectiles", 0, 0, 0, SkillType.ENCHANTING_PIERCING),
+                new Skill("Fire Aspect enchantment", SkillType.ENCHANTING_FIRE_ASPECT),
+                new Skill("Steals health based on outputted DMG", SkillType.ENCHANTING_LIFE_STEAL),
+                new Skill("Adds pierce to projectiles", SkillType.ENCHANTING_PIERCING),
 
                 // Meele sub-trees
-                new Skill("Swords skill tree", 0, 0, 0, SkillType.MEELE_SWORDS),
-                new Skill("Fisticuffs skill tree", 0, 0, 0, SkillType.MEELE_FISTICUFFS),
-                new Skill("Blunt skill tree", 0, 0, 0, SkillType.MEELE_BLUNT),
+                new Skill("Swords skill tree", SkillType.MEELE_SWORDS),
+                new Skill("Fisticuffs skill tree", SkillType.MEELE_FISTICUFFS),
+                new Skill("Blunt skill tree", SkillType.MEELE_BLUNT),
 
                 // Swords weapons
-                new Skill("Short range, medium DMG, medium speed", 0, 0, 0, SkillType.SWORDS_SHORTSWORD),
-                new Skill("2 katanas, high speed, medium range, low dmg", 0, 0, 0, SkillType.SWORDS_DUAL_WIELDING),
-                new Skill("Low atk speed, high attack dmg, high range", 0, 0, 0, SkillType.SWORDS_ZWEIHAENDER),
+                new Skill("Short range, medium DMG, medium speed", SkillType.SWORDS_SHORTSWORD),
+                new Skill("2 katanas, high speed, medium range, low dmg", SkillType.SWORDS_DUAL_WIELDING),
+                new Skill("Low atk speed, high attack dmg, high range", SkillType.SWORDS_ZWEIHAENDER),
 
                 // Fisticuffs weapons
-                new Skill("High speed, low damage, 10% crit", 0, 0, 0, SkillType.FISTICUFFS_LEATHER),
-                new Skill("Medium speed, medium damage, low range, 12% crit", 0, 0, 0, SkillType.FISTICUFFS_METAL_GLOVES),
-                new Skill("Low speed, low range, high DMG, 15% crit", 0, 0, 0, SkillType.FISTICUFFS_SPIKE_GLOVES),
+                new Skill("High speed, low damage, 10% crit", SkillType.FISTICUFFS_LEATHER),
+                new Skill("Medium speed, medium damage, low range, 12% crit", SkillType.FISTICUFFS_METAL_GLOVES),
+                new Skill("Low speed, low range, high DMG, 15% crit", SkillType.FISTICUFFS_SPIKE_GLOVES),
 
                 // Blunt weapons
-                new Skill("Slow attack, medium dmg, stuns on hit, high knockback", 0, 0, 0, SkillType.BLUNT_HAMMER),
-                new Skill("High range, high knockback, slow attack speed", 0, 0, 0, SkillType.BLUNT_MORNINGSTAR),
-                new Skill("High atk speed, very low dmg, very high knockback", 0, 0, 0, SkillType.BLUNT_SPRING_HAMMER),
+                new Skill("Slow attack, medium dmg, stuns on hit, high knockback", SkillType.BLUNT_HAMMER),
+                new Skill("High range, high knockback, slow attack speed", SkillType.BLUNT_MORNINGSTAR),
+                new Skill("High atk speed, very low dmg, very high knockback", SkillType.BLUNT_SPRING_HAMMER),
 
                 // Ranged sub-trees
-                new Skill("Bow skill tree", 0, 0, 0, SkillType.RANGED_BOW),
-                new Skill("Crossbow skill tree", 0, 0, 0, SkillType.RANGED_CROSSBOW),
-                new Skill("Trident skill tree", 0, 0, 0, SkillType.RANGED_TRIDENT),
+                new Skill("Bow skill tree", SkillType.RANGED_BOW),
+                new Skill("Crossbow skill tree", SkillType.RANGED_CROSSBOW),
+                new Skill("Trident skill tree", SkillType.RANGED_TRIDENT),
 
                 // Bow weapons
-                new Skill("Medium range, medium damage, low pierce", 0, 0, 0, SkillType.BOW_BONE_BOW),
-                new Skill("High ATK speed, short range, low dmg, no pierce", 0, 0, 0, SkillType.BOW_SHORTBOW),
-                new Skill("High damage, low speed, high range, high pierce", 0, 0, 0, SkillType.BOW_WAR_BOW),
+                new Skill("Medium range, medium damage, low pierce", SkillType.BOW_BONE_BOW),
+                new Skill("High ATK speed, short range, low dmg, no pierce", SkillType.BOW_SHORTBOW),
+                new Skill("High damage, low speed, high range, high pierce", SkillType.BOW_WAR_BOW),
 
                 // Crossbow weapons
-                new Skill("Shoots twice, low damage, high speed, very low pierce", 0, 0, 0, SkillType.CROSSBOW_DUAL_CROSSBOW),
-                new Skill("Very low speed, very high damage, infinite pierce", 0, 0, 0, SkillType.CROSSBOW_HEAVY_CROSSBOW),
-                new Skill("DOT stackable, low base dmg, medium pierce", 0, 0, 0, SkillType.CROSSBOW_POISON_CROSSBOW),
+                new Skill("Shoots twice, low damage, high speed, very low pierce", SkillType.CROSSBOW_DUAL_CROSSBOW),
+                new Skill("Very low speed, very high damage, infinite pierce", SkillType.CROSSBOW_HEAVY_CROSSBOW),
+                new Skill("DOT stackable, low base dmg, medium pierce", SkillType.CROSSBOW_POISON_CROSSBOW),
 
                 // Trident weapons
-                new Skill("Flies straight, infinite pierce, high damage", 0, 0, 0, SkillType.TRIDENT_HEAVY_TRIDENT),
-                new Skill("Freezes enemies with AOE, slows on meele hit", 0, 0, 0, SkillType.TRIDENT_ICE_TRIDENT),
-                new Skill("Medium range/close damage, no pierce, comes back", 0, 0, 0, SkillType.TRIDENT_RECALL_TRIDENT),
+                new Skill("Flies straight, infinite pierce, high damage", SkillType.TRIDENT_HEAVY_TRIDENT),
+                new Skill("Freezes enemies with AOE, slows on meele hit", SkillType.TRIDENT_ICE_TRIDENT),
+                new Skill("Medium range/close damage, no pierce, comes back", SkillType.TRIDENT_RECALL_TRIDENT),
 
         };
     }
