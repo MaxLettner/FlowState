1d63e730dfc3fa00193da6e9c18a12cceaebf1aa
MaxLettner <lettnermaximilian2008@gmail.com>
2026-04-21 09:24:46 +0200

    fix: fixed module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,8 +12,6 @@ module at.htl.flowstate {
     exports at.htl.flowstate;
     exports at.htl.flowstate.Factories;
     opens at.htl.flowstate.Factories to com.almasb.fxgl.core;
-    opens at.htl.flowstate.Components to com.almasb.fxgl.core;
-    exports at.htl.flowstate.Components;
     exports at.htl.flowstate.Generation;
     opens at.htl.flowstate.Generation to com.almasb.fxgl.core;
     exports at.htl.flowstate.Components.Identifier;
